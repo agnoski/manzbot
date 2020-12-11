e6566a525a869d85fdd7a3c20972f98dbda725ca
agno <crash.pc89@gmail.com>
2020-12-11 23:31:52 +0100

    Remove config loading from InvestersManager

--- app/src/main/groovy/app/InvestersManager.groovy
+++ app/src/main/groovy/app/InvestersManager.groovy
@@ -1,27 +1,21 @@
 package app
 
-import groovy.json.JsonSlurper
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
 class InvestersManager {
   private final int threadDelay = 60
-  private String configFilePath = "/home/agno/Dev/gradleTest/app/app/src/main/groovy/app/config.json"
 
-  public InvestersManager() {
-    // load json
-    def jsonSlurper = new JsonSlurper()
-    def config = jsonSlurper.parse(new File(configFilePath))
-
-    ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(config.indexes.size())
+  InvestersManager(config) {
+    // create thread pool
+    ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(config.assets.size())
 
     // create Investers
-    def investers = config.assets.collect { asset -> 
-      return new Invester(asset.asset, asset.technicalSettings, asset.parameters, asset.timePeriod)
+    def investers = config.assets.collect { Invester(it.asset, it.technicalSettings, it.parameters, it.timePeriod)
     }
 
-    // launch threads
+    // launch Investers threads
     investers.eachWithIndex { invester, index ->
       scheduledThreadPool.scheduleAtFixedRate(invester, index * threadDelay, invester.timePeriod, TimeUnit.SECONDS)
     }
