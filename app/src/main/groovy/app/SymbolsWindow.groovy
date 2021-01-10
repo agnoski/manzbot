0b6eee8b8894a55db83ab899e888b57b275cc661
agno <crash.pc89@gmail.com>
2021-01-10 22:04:07 +0100

    Add displayed check for symbol label

--- app/src/main/groovy/app/SymbolsWindow.groovy
+++ app/src/main/groovy/app/SymbolsWindow.groovy
@@ -3,7 +3,7 @@ package app
 class SymbolsWindow extends BaseWindow {
   static content = {
     symbolsItems { $("div.items") }
-    indexCategory { text -> $("span.label", text: text) }
+    indexCategory { text -> $("span.label", text: text).find { it.displayed } }
   }
 
   void showOrHideCategories(categories) {
